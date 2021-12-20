249c42d91806c631ebf387a27ac73e729e1fbf56
[Rocio Diaz] <sistemasdiazrocio@gmail.com>
2021-12-20 18:10:56 -0300

    clases generadas

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model) :
+    DNI=models.IntegerField()
+    Nombre_Completo=models.CharField(max_length=40)
+    email=models.EmailField()
+    password=models.CharField(max_length=15)
+    #Id_Tipo=models.IntegerField()
+
+class Tipo_Usuario(models.Model):
+    Id_Tipo=models.IntegerField()
+    Tipo_Usuario=models.CharField(max_length=15)
+
